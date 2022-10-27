9709ee134b1d0feefd4c5054759f689ee4bdccdc
Joseph <hushjoezzybad@gmail.com>
2022-10-27 12:15:23 +0100

    serverless commit one

---
+++ backend/src/helpers/todosAcess.ts
@@ -0,0 +1,48 @@
+import * as AWS from 'aws-sdk'
+const AWSXRay = require('aws-xray-sdk')
+import { DocumentClient } from 'aws-sdk/clients/dynamodb'
+// import { createLogger } from '../utils/logger'
+import { TodoItem } from '../models/TodoItem'
+// import { TodoUpdate } from '../models/TodoUpdate';
+
+const XAWS = AWSXRay.captureAWS(AWS)
+
+// const logger = createLogger('TodosAccess')
+
+const todosTable = process.env.TODOS_TABLE
+const docClient: DocumentClient = createDynamoDBClient()
+// // TODO: Implement the dataLayer logic
+export async function createTodo(todo: TodoItem): Promise<TodoItem> {
+    await docClient.put({
+      TableName: todosTable,
+      Item: todo
+    }).promise()
+
+    return todo
+  }
+
+  export async function getAllTodosByUserId (userId: string): Promise<TodoItem> {
+    const result = await docClient.query({
+        TableName : todosTable,
+        KeyConditionExpression: 'userId = :userId',
+        ExpressionAttributeValues: {
+            ':userId': userId
+        }
+    }).promise()
+    const items = result.Items
+    return items as TodoItem[]
+    // return result.Items as TodoItem[]
+  }
+
+  function createDynamoDBClient() {
+    if (process.env.IS_OFFLINE) {
+      console.log('Creating a local DynamoDB instance')
+      return new XAWS.DynamoDB.DocumentClient({
+        region: 'localhost',
+        endpoint: 'http://localhost:8000'
+      })
+    }
+  
+    return new XAWS.DynamoDB.DocumentClient()
+  }
+  
