377081a14e0066fc632d0033bb557d8acb157ddb
Joseph <hushjoezzybad@gmail.com>
2022-11-06 10:03:45 +0100

    seperated logics

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -6,7 +6,8 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 // import { getUserId } from '../utils'
 import { createLogger } from '../../utils/logger'
-import { deleteTodo, getTodoById } from '../../dataLayer/todosAcess'
+// import { deleteTodo, getTodoById } from '../../dataLayer/todosAcess'
+import { deleteTheTodo } from '../../businessLogic/todos'
 // import { deleteTodo } from '../../logicLayer/todos'
 const logger = createLogger('Todo')
 
@@ -14,9 +15,8 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
   logger.warn("Processing delete event on todo", event )
   const todoId = event.pathParameters.todoId
 
-  const todo = await getTodoById(todoId)
   // TODO: Remove a TODO item by id
-await deleteTodo(todo)
+await deleteTheTodo(todoId)
   return {
     statusCode: 201,
     headers: {
